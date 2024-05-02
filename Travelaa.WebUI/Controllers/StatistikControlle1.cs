ee124fec6f510df9e75d54eb4381b2c35b528c9f
tuba balkan <tubabalkan165@gmail.com>
2024-05-02 22:02:21 +0300

    ekleme yapıldı

---
+++ Travelaa.WebUI/Controllers/StatistikControlle1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using Travelaa.WebUI.Dtos;
+
+namespace Travelaa.WebUI.Controllers
+{
+    public class StatistikControlle1 : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StatistikControlle1(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7113/api/Category/CategoryCount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.c = jsonData;
+            return View();
+        }
+    }
+}
